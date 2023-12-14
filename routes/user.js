b526c6c1ccd7b7c45e9658b468137082c7ee331e
KaranChawla <chawlakaran663@gmail.com>
2023-12-14 15:02:01 +0530

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  getAllUsers,
+  register,
+  login,
+  getMyProfile,
+  logout,
+} from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import { deleteTask, updateTask } from "../controllers/task.js";
+
+const router = express.Router();
+
+router.get("/all", getAllUsers);
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/profile", isAuthenticated, getMyProfile);
+
+router.get("/logout", logout);
+
+
+export default router;
